08227fcde3b49b1c04f7451d57af7b99bbdabdea
Maria Pushkova <mariapushkova95@gmail.com>
2021-10-01 00:28:53 +0300

    HW

--- Lesson_1/Home_work/index.js
+++ Lesson_1/Home_work/index.js
@@ -5,9 +5,9 @@ let myString = "A string";
 let isCashPayment = true;
 let myUndefined;
 let carQuantity = null;
-let user = {
-  name: "Maria",
-  age: 25,
+let city = {
+  name: "Nizhny Novgorod",
+  age: 800,
 };
 let id = Symbol("id");
 let bigIntNumber = 1n;
@@ -17,7 +17,7 @@ console.log(myString);
 console.log(isCashPayment);
 console.log(myUndefined);
 console.log(carQuantity);
-console.log(user);
+console.log(city);
 console.log(id);
 console.log(bigIntNumber);
 
@@ -26,6 +26,31 @@ console.log(typeof myString);
 console.log(typeof isCashPayment);
 console.log(typeof myUndefinied);
 console.log(typeof carQuantity);
-console.log(typeof user);
+console.log(typeof city);
 console.log(typeof id);
 console.log(typeof bigIntNumber);
+
+let city1 = {
+  name: "Nizhny Novgorod",
+  age: 800,
+  age: 900,
+};
+console.log(city1);
+
+var city2 = {
+  name: "Nizhny Novgorod",
+  age: 800,
+  age: 900,
+};
+console.log(city2);
+
+const city3 = {
+  name: "Nizhny Novgorod",
+  age: 800,
+  name: "Moscow",
+};
+console.log(city3);
+
+const name = "Maria";
+name = "Alex";
+console.log(name);
