5cb0aff9bce5efd2068034b580c59a63eb730967
Maria Pushkova <mariapushkova95@gmail.com>
2021-09-30 23:49:07 +0300

    ДЗ

---
+++ lesson 1 homework/index.js
@@ -0,0 +1,36 @@
+1.console.log("Hello world");
+
+
+2.	let myNumber = 500;
+let myString = "A string";
+let isCashPayment = true;
+let myUndefined;
+let carQuantity = null;
+let user = {
+    name: "Maria",
+    age: 25
+};
+let id = Symbol("id");
+let bigIntNumber = 1n;
+
+3.	console.log(myNumber);
+console.log(myString);
+console.log(isCashPayment);
+console.log(myUndefined);
+console.log(carQuantity);
+console.log(user);
+console.log(id);
+console.log(bigIntNumber);
+
+4.	console.log(typeof myNumber);
+console.log(typeof myString);
+console.log(typeof isCashPayment);
+console.log(typeof myUndefinied);
+console.log(typeof carQuantity);
+console.log(typeof user);
+console.log(typeof id);
+console.log(typeof bigIntNumber);
+
+5.	const name = "Maria";
+name = "Alex";
+console.log(name);
