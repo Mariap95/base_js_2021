d755c17e18691afe86fb182c2eb63a214fa959bb
Maria Pushkova <mariapushkova95@gmail.com>
2021-10-05 00:45:13 +0300

    урок 3

---
+++ Lesson_3/Class_work/index.js
@@ -0,0 +1,71 @@
+//const firstValue = String(5);
+//* console.log(firstValue);
+//console.log(typeof firstValue);
+
+//const obj = {
+//  foo: "bar",
+// age: 25,
+//};
+
+//console.log(typeof String(obj));*//;
+//console.log(String(obj.foo));
+// console.log(String(obj.age));
+
+//NUMBER
+
+/*let testValue = null;
+console.log(typeof testValue);
+testValue = Number(testValue);
+console.log(testValue);*/
+
+/*const isUser = true;
+const isAdmin = false;
+console.log(Number(isUser), Number(isAdmin));
+
+const voidString = "";
+console.log(Number(voidString));*/
+
+// BOOLEAN
+
+// const value = "0";
+
+// console.log(typeof Boolean(value), Boolean(value));
+
+// const obj = [].length;
+// console.log(Boolean(obj));
+
+// console.log(2 ** 2);
+
+//Унарный плюс
+
+// const value = 5;
+// console.log(typeof value, typeof +value);
+
+// const value = "5";
+// console.log(typeof value, typeof +value, typeof Number(value));
+
+//Бинарный плюс
+
+// let sum = 5 + 6;
+// console.log(sum);
+
+// sum = 1 + true;
+// console.log(sum);
+// sum = 5 + null;
+// console.log(sum);
+
+// const stringValue = "12";
+// const numberValue = 34;
+// console.log(+stringValue + numberValue);
+// console.log(numberValue + +stringValue);
+
+const firstName = "Maria";
+const lastName = "Pushkova";
+
+console.log(firstName + " " + lastName);
+console.log(`${firstName} ${lastName}`);
+
+console.log(4 + 5 + "O");
+console.log("2" * "3");
+console.log("4" - 2);
+console.log("4" - "4x");
