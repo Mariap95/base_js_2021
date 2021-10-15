478c06049d51633d3e55a9eaf7a2aceeb9733b3d
Maria Pushkova <mariapushkova95@gmail.com>
2021-10-15 20:41:10 +0300

    15.10

---
+++ Lesson_4/Class_work/index.js
@@ -0,0 +1,186 @@
+// if (1) {
+//   console.log("TRUE");
+// }
+// if (0) {
+//   console.log("FALSE");
+// }
+// console.log("START");
+// const value = -5;
+// if (value > 0) {
+//   console.log("TRUE");
+// } else {
+//   console.log("FALSE");
+// }
+
+// console.log("FINISH");
+
+// console.log("Start");
+
+// let value;
+// if (value === 0) {
+//   console.log(value, " = value");
+// } else if (value > 1) {
+//   console.log(value, " > 1");
+// } else if (value < -1) {
+//   console.log(value, " < -1");
+// } else {
+//   console.log(value);
+// }
+// console.log("Finish");
+
+// const age = 12;
+// const result = age >= 18 ? "Доступн разрешен" : "Доступ запрещен";
+// console.log(result);
+
+// switch ("Ivan") {
+//   case "Ivan":
+//     console.log("IVAN");
+//     break;
+//   case "Alex":
+//     console.log("ALEX");
+//     break;
+//   case "Max":
+//     console.log("MAX");
+//     break;
+//   default:
+//     console.log("Имени нет в списке");
+// }
+// console.log("FINISH");
+
+// let a = 5;
+// let b = 10;
+// let c = 1;
+// if ((a > b) & (a > c)) {
+//   console.log(a);
+// } else if ((b > a) & (b > c)) {
+//   console.log(b);
+// } else if ((c > a) & (c > b)) {
+//   console.log(c);
+// }
+// let temperature = 10;
+// if (temperature < -30) {
+//   console.log("Оставайтесь дома!");
+// } else if (-30 <= temperature <= -10) {
+//   console.log("Сегодня холодно");
+// } else if (-10 < temperature <= +5) {
+//   console.log("Не холодно");
+// } else if (+5 < temperature <= +15) {
+//   console.log("Тепло");
+// } else if (+15 < temperature <= +25) {
+//   console.log("Очень тепло");
+// } else if (+25 < temperature <= +35) {
+//   console.log("Жарко");
+// } else if (temperature > +35) {
+//   console.log("Пекло!");
+// }
+
+// const userRole = "user";
+// switch (userRole) {
+//   case "user":
+//     console.log("user_info");
+//     break;
+//   case "manager":
+//     console.log("manager info");
+//     break;
+//   case "manager":
+//     console.log("1991");
+//     break;
+//   default:
+//     console.log("no info");
+//     break;
+// }
+
+// const a = 100;
+// const b = 1;
+// const result1 = a || b;
+// console.log(result1);
+
+// const a = 10;
+// const b = "";
+// const result = a && b;
+// console.log(result);
+
+// const a = 10;
+// const b = 200;
+// const c = 3;
+// const d = 1000;
+// // if (a > b && a > c) {
+// //   console.log(a);
+// // } else if (b > a && b > c) {
+// //   console.log(b);
+// // } else {
+// //   console.log(c);
+// // }
+// console.log((a && b) || (c && d));
+
+// const name = "";
+// if (!name) {
+//   console.log("Имя нет");
+// } else {
+//   console.log("Имя есть");
+// }
+
+// console.log(!!"Alex");
+// console.log(Boolean("Alex"));
+
+// console.log("start");
+// let i = 0;
+// while (true) {
+//   console.log("loop");
+//   i += 1;
+//   if (i > 10) break;
+// }
+// console.log("end");
+
+// let i = 0;
+// while (i < 10) {
+//   console.log(i);
+//   i += 1;
+// //   i = i + 1;
+// }
+// console.log("end");
+
+// do {
+//   console.log("WORK");
+// } while (i);
+// console.log("end");
+// let i = 1;
+// while (i <= 10) {
+//   console.log("Maria");
+//   i += 1;
+// }
+
+// for (let i = 1; i <= 10; i += 1) {
+//   console.log("Maria");
+// }
+
+// while (k < j) {
+//   let i = 500;
+//   let k = i / 2;
+//   let j = 67;
+
+//   let l = 1;
+//   l += 1;
+//   console.log(l);
+// }
+// for (let i = 500; i < 67; i / 2) {
+//   console.log(i);
+// }
+
+// let M = 100;
+// let N = 26;
+// let days = 0;
+// while (M >= N) {
+//   M /= 2;
+//   days += 1;
+// }
+// console.log(days);
+// let i = 1800;
+// let j = 2020;
+// let count = 1;
+// while (i <= j) {
+//   i += 1;
+//   count += 1;
+//   if ((i = 1961)) break;
+// }
+// console.log(count);
