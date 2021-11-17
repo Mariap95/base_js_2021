ef31c80daf03a8ef10d8e7796a64e5900343cee4
Maria Pushkova <mariapushkova95@gmail.com>
2021-11-18 00:23:15 +0300

    hw

--- Lesson_7/Home_work/index.js
+++ Lesson_7/Home_work/index.js
@@ -1,90 +1,76 @@
 // 1
 // let vegetables = ["Капуста", "Репа", "Редиска", "Морковка"];
 
-// let vegetableLength = vegetables.map(function (piece) {
-//   return piece.length;
-// });
-
-// console.log(vegetableLength);
+// let getLength = (arr) =>
+//   arr.map((el) => {
+//     return el.length;
+//   });
+// console.log(getLength(vegetables));
 
 // 2
 // let numbers = [2, 3, 5, 7, 11, 13, 17, 19];
 // function currentSums(numbers) {
 //   let result = [];
-//   numbers.reduce(function (sum, current, i) {
-//     return (result[i] = sum + current);
-//   }
-//   console.log(result);
-// }
-// currentSums(numbers);
-
-// 3
-// let numbers = [0, 1, 2, 3, 4, 5, 6, 7];
-// function sumSeven(numbers) {
-//   let obj = {};
-//   let result = [];
-
-//   for (let i = 0; i < numbers.length; i++) {
-//     let elem = numbers[i];
-// }
-//     for (let j = i + 1; j < numbers.length; j++) {
-//       let item = numbers[j];
-// }
-//       if (elem + item != 7) {
-//      continue;
-// }
-//       let num = elem + ":" + item;
-//       obj[num] = num;
-//     }
-//   }
-
-//   for (let key in obj) {
-//     result.push(obj[key]);
-//   }
+//   result.push(numbers[0]);
+//   numbers.reduce((accom, current, i) => {
+//     return (result[i] = accom + current);
+//   });
 //   return result;
 // }
 
-// console.log(sumSeven(numbers));
+// console.log(currentSums(numbers));
+
+// 3
+// let arr = [0, 1, 2, 3, 4, 5, 6, 7];
+// let sumSeven = (arr) => {
+//   let pairsArr = [];
+//   for (let i = 0; i < arr.length; i++) {
+//     for (let j = i + 1; j < arr.length; j++) {
+//       if (arr[i] + arr[j] === 7) {
+//         pairsArr.push(arr[i] + ":" + arr[j]);
+//       }
+//     }
+//   }
+//   return pairsArr;
+// };
+// console.log(sumSeven(arr));
 
 // 4
 // let str = "Каждый охотник желает знать, где сидит фазан.";
+// let getFirstLetter = (str) => {
+//   let newArr = str.split(" ");
+//   let firstLetter = [];
+//   for (let i = 0; i < newArr.length; i++) {
+//     firstLetter.push(newArr[i].slice(0, 1));
+//   }
 
-// function firstChar(value, index, arr) {
-//   if (index == 0) {
-//   return true;
-//   } else {
-//   return arr[index - 1] === " ";
-// }
-// }
-
-// let array = [].filter.call(str, firstChar);
-
-// console.log(array);
+//   return firstLetter;
+// };
+// console.log(getFirstLetter(str));
 
 // 5
 // let str = "JavaScript";
-
-// function getChars(value, index, str) {
-//   return str.substring(index - 1, index + 2);
-// }
-
-// let arr = Array.prototype.map.call(str, getChars);
-
-// console.log(arr);
+// let getArr = (str) => {
+//   let arr = [];
+//   for (let i = 0; i < str.length; i++) {
+//     if (str[i - 1] == undefined) {
+//       arr.push(str[i] + str[i + 1]);
+//     } else if (str[i + 1] == undefined) {
+//       arr.push(str[i - 1] + str[i]);
+//     } else {
+//       arr.push(str[i - 1] + str[i] + str[i + 1]);
+//     }
+//   }
+//   return arr;
+// };
+// console.log(getArr(str));
 
 // 6
 // let numerics = [5, 7, 2, 9, 3, 1, 8];
-// function matching(first, second) {
-//   if (first == second) {
-//  return 0;
-//  } else if (first > second) {
-//  return -1;
-//  } else {
-//  return 1;
-// }
-// }
-// numerics = numerics.sort(matching);
-// console.log(numerics);
+// let foo = (arr) => {
+//   return arr.sort((a, b) => b - a);
+// };
+// console.log(foo(numerics));
 
 // 7
 // let a = [1, 2, 3];
@@ -133,34 +119,37 @@
 // console.log(sum);
 
 // 10
-// function arr(a) {
-//   let b = [];
-//   for (let i = a.length - 1; i >= 0; i--) {
-//     b.push(a[i]);
+// let arr = [1, 4, 5, 6, 7, 3];
+// let reverseArr = (arr) => {
+//   let arr2 = [];
+//   for (let i = arr.length - 1; i >= 0; i--) {
+//     arr2.push(arr[i]);
 //   }
-//   return b;
-// }
-// console.log(arr[(1, 5, 6, 7)]);
+//   return arr2;
+// };
+// console.log(reverseArr(arr));
 
 // 11
-// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-// let sum = 0;
-// for (let i = 0; i < arr.length; i++) {
-//   sum += arr[i];
-//   if (sum > 10) {
-//     break;
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 67];
+// let getSum = (arr) => {
+//   let sum = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//     sum += arr[i];
+//     if (sum > 10) {
+//       console.log(i + 1);
+//       break;
+//     }
 //   }
-// }
-// console.log(i + 1);
+// };
+
+// console.log(getSum(arr));
 
 // 12
-// let arr = [];
-// let data;
-// let kil;
-// function arrayFill(data, kil) {
-//   for (let i = 0; i < kil; i++) {
-//     arr[i] = data;
-//     console.log(arr[i]);
+// let arrayFill = (data, num) => {
+//   let arr = [];
+//   for (let i = 0; i < num; i++) {
+//     arr.push(data);
 //   }
-// }
-// arrayFill("x", 10);
+//   return arr;
+// };
+// console.log(arrayFill(4, 5));
